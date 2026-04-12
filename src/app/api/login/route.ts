68875d01debddb193633acc1811f11be82c99adf
victo <victorcordeirofp@gmail.com>
2026-04-12 18:16:19 -0300

    refactor: criando functions de validação e cookie

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,20 +1,28 @@
-"use server"
 import {NextRequest, NextResponse} from "next/server";
 import { cookies } from "next/headers";
 
-export async function POST(req: NextRequest, res: NextResponse){
-    const cookie = await cookies();
-    const pushCookie = cookie.get("authToken")?.value
-    console.log("linha 8: ", pushCookie);
+function validateBody(body: {email?: string, password?: string}) {
+    if(!body.email && !body.password) return NextResponse
+        .json({ message: "Preencha todos os campos!" }, { status: 400 });
+}
+
+function applyCookies(res: NextResponse, backRes: Response) {
+    const cookieHeader = backRes.headers.getSetCookie();
+    if(cookieHeader && cookieHeader.length > 0) {
+        cookieHeader.forEach(cookie => {
+            res.headers.append("Set-Cookie", cookie);
+        })
+    }
+}
+
+export async function POST(req: NextRequest) {
     try {
         const body = await req.json() as {
             email?: string;
             password?: string;
         };
-        const { email, password } = body;
 
-        if(!email && !password) return NextResponse
-            .json({ message: "Preencha todos os campos!" }, { status: 400 });
+        validateBody(body);
 
         const backRes = await fetch("http://localhost:8080/api/login/enter", {
             method: "POST",
@@ -27,16 +35,12 @@ export async function POST(req: NextRequest, res: NextResponse){
         if(!backRes.ok) return NextResponse
             .json({ message: "Credênciais inválidas! Tente novamente." }, { status: 401 });
 
+        const res = NextResponse.json(await backRes.json());
 
-        const cookieHeader = backRes.headers.getSetCookie();
+        applyCookies(res, backRes);
 
-        if(cookieHeader && cookieHeader.length > 0) {
-            cookieHeader.forEach(cookie => {
-                res.headers.append("Set-Cookie", cookie);
-            })
-        }
         return res;
-    } catch {
-        return NextResponse.json({message: 'Erro ao realizar login! Tente novamente.'}, {status: 500});
+    } catch (e) {
+        return NextResponse.json({ message: "Erro ao realizar login!" }, { status: 500 });
     }
 }
