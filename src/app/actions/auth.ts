ccda48b411e8d4540d448a9218f505aa69e23e24
victo <victorcordeirofp@gmail.com>
2026-04-14 11:22:31 -0300

    refactor: botao de logout agora se comunica com o backend para inutilizar o token

--- src/app/actions/auth.ts
+++ src/app/actions/auth.ts
@@ -4,10 +4,18 @@ import { redirect } from "next/navigation";
 
 export async function logout() {
     const cookie = await cookies();
+    const result = await fetch("http://localhost:8080/api/login/logout", {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "authorization": `Bearer ${cookie.get("authToken")?.value}`
+        }
+    });
     cookie.set("authToken", "" ,{
         httpOnly: true,
         expires: new Date(0),
         path: "/"
     });
+    console.log(result);
     redirect("/");
 }
